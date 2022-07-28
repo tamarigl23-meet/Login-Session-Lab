c6b4ec5849c340eddf53ed523ad175073fb320cb
tamarigl23-meet <109900070+tamarigl23-meet@users.noreply.github.com>
2022-07-28 16:44:21 +0300

    login session added

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,10 +4,19 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods=['GET', 'POST'] ) # What methods are needed?
 def home():
-	
-	return render_template('home.html')
+	if request.method == 'POST':
+		try:
+			login_session['quote'] = request.form['quote']
+			login_session['author_quote'] = request.form['author_quote']
+			login_session['author_age'] = request.form['author_age']
+			return render_template('thanks.html')
+		except:
+			return render_template('error.html')
+
+	else:
+		return render_template('home.html')
 
 
 @app.route('/error')
@@ -19,7 +28,7 @@ def error():
 @app.route('/display')
 def display():
 
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html',quote1=login_session['quote'], author_quote1=login_session['author_quote'], author_age1 =login_session['author_age']) # What variables are needed?
 
 
 @app.route('/thanks')
